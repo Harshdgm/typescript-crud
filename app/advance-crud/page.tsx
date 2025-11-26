cf45eb051f4206f2a56489607b1f5f59c6b80f2e
harshdgm <harshvaddoriya.dgm@gmail.com>
2025-11-26 18:18:09 +0530

    remove unnecessary code

--- app/advance-crud/page.tsx
+++ app/advance-crud/page.tsx
@@ -7,14 +7,12 @@ import { useUsersStore, initUsersFromAPI, addUsers } from "@/utils/usersStore";
 import { fetchUserData } from "@/lib/api";
 
 export default function UsersAdvancedPage() {
-  // Read from sessionStorage (reactive)
   const usersData = useUsersStore();
 
-  // Load initial API users once
   useEffect(() => {
     async function loadInitial() {
       const apiUsers = await fetchUserData();
-      initUsersFromAPI(apiUsers); // Adds API users into sessionStorage
+      initUsersFromAPI(apiUsers); 
     }
     loadInitial();
   }, []);
