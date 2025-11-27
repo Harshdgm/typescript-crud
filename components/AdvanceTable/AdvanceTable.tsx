527e064c6ff6d002c8ab3a886397cdf68463aa63
harshdgm <harshvaddoriya.dgm@gmail.com>
2025-11-27 15:14:57 +0530

    add camelcase functionality

--- components/AdvanceTable/AdvanceTable.tsx
+++ components/AdvanceTable/AdvanceTable.tsx
@@ -4,6 +4,7 @@
 import { UserData } from "@/types/user";
 import { deleteUserById } from "@/utils/usersStore"; 
 // import { fetchUserData } from "@/lib/api";
+import { toCamelCase } from "@/utils/toCamelCase";
 import Image from "next/image";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
@@ -16,7 +17,6 @@ export default function AdvanceTable({ data }: Props) {
     // const usersData: UserData[] = use(
     //   useMemo(() => fetchUserData(), [])
     // );
-
   return (
     <div>
         <div className="overflow-x-auto text-center">
@@ -34,7 +34,7 @@ export default function AdvanceTable({ data }: Props) {
                     <div className="p-2 border  rounded-tl-2xl rounded-bl-2xl">{user.id}</div>
                     <div className="p-2 border">{user.email}</div>
                     <div className="p-2 border">{user.phone}</div>
-                    <div className="p-2 border">{user.address}</div>
+                    <div className="p-2 border">{toCamelCase(String(user.address ?? ""))}</div>
                     <div className="p-1 border flex items-center justify-center">
                         <Image
                             src={user.image}
@@ -44,7 +44,7 @@ export default function AdvanceTable({ data }: Props) {
                             className="w-16 h-10 object-cover rounded"
                         />
                     </div>
-                    <div className="p-2 border">{user.hobby}</div>
+                    <div className="p-2 border">{toCamelCase(String(user.address ?? ""))}</div>
                     <div className="p-2 border rounded-tr-2xl rounded-br-2xl flex items-center justify-center"><RiDeleteBin6Line className="text-red-500 h-5 w-5" onClick={() => deleteUserById(user.id)} /></div>
                 </div>
             ))}
