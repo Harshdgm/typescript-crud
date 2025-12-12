11c0f41856ff13316737dd00992646c7750d3f63
harshdgm <harshvaddoriya.dgm@gmail.com>
2025-12-12 14:44:28 +0530

    timer utils changes

--- utils/calculateDuration.ts
+++ utils/calculateDuration.ts
@@ -15,22 +15,22 @@ export const calculateDurations = (distance: number): Record<VehicleName, number
 };
 
 
-// export const formatTime = (sec: number): string => {
-//   if (sec >= 86400) { // 24 * 3600
-//     const days = Math.floor(sec / 86400);
-//     const hours = Math.floor((sec % 86400) / 3600);
-//     return `${days} day${days > 1 ? "s" : ""} ${hours} hr`;
-//   }
-//   if (sec >= 3600) {
-//     return `${Math.floor(sec / 3600)} hr ${Math.floor((sec / 60) % 60)} min`;
-//   }
-//   return `${Math.floor(sec / 60)} min`;
-// };
-
-export const formatTime = (ms: number) => {
-  const totalSeconds = Math.floor(ms / 1000);
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = totalSeconds % 60;
-  return `${hours}h ${minutes}m ${seconds}s`;
+export const formatTime = (sec: number): string => {
+  if (sec >= 86400) { // 24 * 3600
+    const days = Math.floor(sec / 86400);
+    const hours = Math.floor((sec % 86400) / 3600);
+    return `${days} day${days > 1 ? "s" : ""} ${hours} hr`;
+  }
+  if (sec >= 3600) {
+    return `${Math.floor(sec / 3600)} hr ${Math.floor((sec / 60) % 60)} min`;
+  }
+  return `${Math.floor(sec / 60)} min`;
 };
+
+// export const formatTime = (ms: number) => {
+//   const totalSeconds = Math.floor(ms / 1000);
+//   const hours = Math.floor(totalSeconds / 3600);
+//   const minutes = Math.floor((totalSeconds % 3600) / 60);
+//   const seconds = totalSeconds % 60;
+//   return `${hours}h ${minutes}m ${seconds}s`;
+// };
